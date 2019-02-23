ac8c78f691e43521d17b3b4eff2933208b7d77b5
Alastar Corr <house.labs@gmail.com>
2019-02-23 15:01:23 +0600

    Github authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
 
+  has_many :authorizations, dependent: :destroy
   has_many :questions
   has_many :answers
   has_many :rewards
   has_many :votes, dependent: :destroy
   has_many :comments
+
+  def self.find_for_oauth(auth)
+    Services::FindForOauth.new(auth).call
+  end
 
   def author?(obj)
     id == obj.user_id

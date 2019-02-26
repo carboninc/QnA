d3e3dbac0adc10f73a48a623ca9a9c0553221d74
Alastar Corr <house.labs@gmail.com>
2019-02-26 15:44:48 +0600

    Doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  use_doorkeeper
   root to: 'questions#index'
   devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks' }
 

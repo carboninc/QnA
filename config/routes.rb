e1929b622918d03cd9b7c844e4d6a5d94651c3c5
Alastar Corr <house.labs@gmail.com>
2019-02-26 22:42:10 +0600

    First API

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,12 @@ Rails.application.routes.draw do
   resources :rewards, only: :index
 
   mount ActionCable.server => '/cable'
+
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
 end

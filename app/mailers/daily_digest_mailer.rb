1372d45c58870503d57525dad30409793e717561
Alastar Corr <house.labs@gmail.com>
2019-03-08 16:40:23 +0600

    Fix mailers

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -6,7 +6,7 @@ class DailyDigestMailer < ApplicationMailer
   #   en.daily_digest_mailer.digest.subject
   #
   def digest(user)
-    @greeting = "Hi #{user.name}"
+    @greeting = "Hi subscriber"
     @questions = Question.last_day
 
     mail to: user.email, subject: 'Daily digest'
